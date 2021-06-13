67bc300c1264b351f8166e4dd46ee8e46a9c3832
Muhammed Güven <mguvenn96@gmail.com>
2021-06-13 16:37:14 +0300

    Splash screen and  icon added

--- TopHundredChallenge/TopHundredChallenge.Android/MainActivity.cs
+++ TopHundredChallenge/TopHundredChallenge.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace TopHundredChallenge.Droid
 {
-    [Activity(Label = "TopHundredChallenge", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TopHundredChallenge", Icon = "@drawable/logo", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
